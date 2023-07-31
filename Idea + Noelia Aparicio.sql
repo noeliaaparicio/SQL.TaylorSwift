6d0b1a226b1ae91e8c78652dbc23026ab4c2c77c
Noe <137910388+noeliaaparicio@users.noreply.github.com>
2023-07-31 20:23:02 -0300

    Limpieza de datos mediante SQL
    En esta base de datos se realizaron las siguientes tareas: 
    1- Consultar para visualizar el conjunto de datos completo.
    2- Identificar celdas con valores faltantes (NULL) en la columna 'acousticness'.
    3- Realizar un recuento del número de celdas con valores faltantes en la columna 'acousticness'.
    4- Rellenar  los valores faltantes en la columna 'acousticness' utilizando valores proporcionados manualmente basados en identificadores únicos (id) de las canciones.
    5- Cambiar el nombre de la primera columna ('column1') a 'number'.
    6- Ajustar el número de inicio en la columna 'number' para que comience desde 1 en lugar de 0.
    7- Comprobar los valores máximo y mínimo en algunas columnas para asegurar que estén dentro del rango esperado.
    8- Cambiar el formato de la columna 'duration' de milisegundos (ms) a minutos y segundos (mm:ss).
    9- Crear una nueva columna 'note' separando los comentarios después del guión en los nombres de las canciones.
    10- Eliminar la columna 'duration_ms' para reducir redundancia y utilizar la columna 'duration' recién creada.
    Estas tareas implicaron limpieza y transformación de datos para mejorar la presentación del conjunto de datos, lo que conlleva a un análisis más efectivo.

---
+++ Idea + Noelia Aparicio.sql
@@ -0,0 +1,8 @@
+SELECT
+    *
+FROM
+    taylor_swift_spotify (1).csv
+## La cláusula ORDER BY se utilizó para verificar que el conjunto de datos estuviera ordenado por la columna 1 (renombrada en pasos posteriores) en orden ascendente.
+ORDER BY
+    number;
+*/
